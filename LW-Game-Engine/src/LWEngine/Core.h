ca505d1867d2f230555a800642a3e63bf7b69ef8
lordwile <lordwilee@outlook.com>
2023-04-20 18:07:59 +0300

    event system added

--- LW-Game-Engine/src/LWEngine/Core.h
+++ LW-Game-Engine/src/LWEngine/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error LWEngine only supports Windows
 #endif
+
+#define BIT(x) (1<<x)
---
+++ LW-Game-Engine/src/LWEngine/Events/KeyEvent.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace LWEngine
+{
+	//. Base class for all events. Constructor is protected so it is kind of abstract class	
+	class LWE_API KeyEvent : public Event
+	{
+	public: 
+		inline int GetKeyCode() const { return m_KeyCode; }
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode){}
+
+		int m_KeyCode;
+	};
+
+	//. KeyPressed class for holding the key long time
+	class LWE_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_repeatCount(repeatCount) {}
+		inline int GetRepeatCount() const { return m_repeatCount; }
+		
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent " << m_KeyCode << " (" << m_repeatCount << " times)";
+			return ss.str();
+		}
+		
+		EVENT_CLASS_TYPE(KeyPressed);
+	private:
+		int m_repeatCount;
+	};
+
+	class LWE_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode){}
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased);
+
+	};
+}
