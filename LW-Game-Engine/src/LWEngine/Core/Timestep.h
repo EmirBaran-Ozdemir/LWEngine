2392c358220a183b4a638c206c89dfe3eac4ef1e
LordWile <lordwilee@outlook.com>
2023-06-27 16:37:28 +0300

    Elapsed time moved to Timestep.h

--- LW-Game-Engine/src/LWEngine/Core/Timestep.h
+++ LW-Game-Engine/src/LWEngine/Core/Timestep.h
@@ -5,16 +5,18 @@ namespace LWEngine {
 	class Timestep
 	{
 	public:
-		Timestep(float time = 0.0f)
-			: m_Time(time)
+		Timestep(float time = 0.0f, float elapsedTime = 0.0f)
+			: m_Time(time), m_ElapsedTime(elapsedTime)
 		{
 		}
 		operator float() const { return m_Time; }
 
 		float GetSeconds() const { return m_Time; }
 		float GetMiliseconds() const { return m_Time * 1000.0f; }
+		float GetElapsedTime() const { return m_ElapsedTime; }
 		
 	private:
 		float m_Time;
+		float m_ElapsedTime;
 	};
 }
